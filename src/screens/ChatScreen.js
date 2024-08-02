6e5c12cb7aa5611b29259d0bb94ca7788de2ad33
masielmartinez <msl.mrtnz.007@gmail.com>
2024-08-02 13:59:40 -0700

    Update ChatScreen.js
    created a function called disclaimer
    created state vars called disclaimerVisible and actionVisible

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -10,32 +10,37 @@ import { CHATBOTS } from "./ConversationScreen";
 import AddEvent from "../components/AddEvent";
 import Actions from "../components/Actions";
 import PinnedBotBitmoji from "../components/PinnedBotBitmoji";
+import Disclaimer from "../components/Disclaimer";
 
 export default function ChatScreen({ navigation }) {
   const [chats, setChats] = useState([]);
   const [detailsVisible, setDetailsVisible] = useState(false);
-  const [visible, setVisible] = useState(false);
+  const [actionsVisible, setActionsVisible] = useState(false);
+  const [disclaimerVisible, setDisclaimerVisible] = useState(false);
   const [selectedEvent, setSelectedEvent] = useState(null);
   const insets = useSafeAreaInsets();
   const tabBarHeight = useBottomTabBarHeight();
 
-  function toggleComponent() {
-    console.log("Pressed Star");
-    setVisible(!visible);
-    console.log(visible);
+  function toggleActions() {
+    console.log("Toggle Actions");
+    setActionsVisible(!actionsVisible);
+  }
+
+  function toggleDisclaimer() {
+    console.log("Toggle Disclaimer");
+    setDisclaimerVisible(!disclaimerVisible);
   }
 
   function handleCardTouch(event) {
     setDetailsVisible(true);
-    console.log(detailsVisible);
     setSelectedEvent(event);
   }
+
   function getChatbots() {
     let chatbotsTemp = [];
     for (const botId in CHATBOTS) {
       chatbotsTemp.push({ isChatbot: true, chatId: botId });
     }
-
     setChats((otherChats) => [...otherChats, ...chatbotsTemp]);
   }
 
@@ -44,7 +49,6 @@ export default function ChatScreen({ navigation }) {
       getChatbots();
     }
   }, [chats.length]);
-
   return (
     <View
       style={[
@@ -73,13 +77,14 @@ export default function ChatScreen({ navigation }) {
           <PinnedBotBitmoji name={"MyAI"}></PinnedBotBitmoji>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("Conversation", {
-              isChatbot: false,
-              chatId: 1,
-            });
-          }}
-          key={1}
+          onPress={toggleDisclaimer}
+          // {() => {
+          //   navigation.navigate("Conversation", {
+          //     isChatbot: false,
+          //     chatId: 1,
+          //   });
+          // }}
+          // key={1}
         >
           <PinnedBotBitmoji
             name={"MyWellness"}
@@ -116,12 +121,12 @@ export default function ChatScreen({ navigation }) {
           </TouchableOpacity>
         ))}
       </View>
-      <FAB // Button for bring up dialogue
+      <FAB //initiatives button
         style={styles.addButton}
         visible={true}
         icon={{ name: "star", color: "white" }}
         color="#FF3386"
-        onPress={toggleComponent}
+        onPress={toggleActions}
       />
       <FAB
         style={styles.addButtonSecond}
@@ -130,11 +135,15 @@ export default function ChatScreen({ navigation }) {
         color="#3CB2E2"
       />
       <Actions
-        isVisible={visible}
-        onClose={() => {
-          toggleComponent();
-        }}
+        isVisible={actionsVisible}
+        onClose={toggleActions}
       />
+      <Disclaimer
+        isVisible={disclaimerVisible}
+        onClose={toggleDisclaimer}
+      />
+
+      
     </View>
   );
 }
