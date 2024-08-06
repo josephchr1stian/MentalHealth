9a5fa4edc386668029b57ae9c0b6e64f41e357db
masielmartinez <msl.mrtnz.007@gmail.com>
2024-08-06 00:05:19 -0700

    Update SendBottomSheet.js

--- src/components/SendBottomSheet.js
+++ src/components/SendBottomSheet.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { BottomSheet, ListItem } from "@rneui/themed";
-import { StyleSheet } from "react-native";
+import { Modal, View, TouchableOpacity, StyleSheet } from "react-native";
+import { ListItem } from "@rneui/themed";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 const SendBottomSheet = ({ showMenu, setShowMenu }) => {
@@ -14,34 +14,54 @@ const SendBottomSheet = ({ showMenu, setShowMenu }) => {
   ];
 
   return (
-    <BottomSheet
-      modalProps={{
-        transparent: true,
-        animationType: "slide",
-        visible: showMenu,
-        onRequestClose: () => setShowMenu(false),
-      }}
-      isVisible={showMenu}
-      containerStyle={{ backgroundColor: "transparent" }}
+    <Modal
+      transparent={true}
+      animationType="slide"
+      visible={showMenu}
+      onRequestClose={() => setShowMenu(false)}
     >
-      {list.map((l, i) => (
-        <ListItem key={i} containerStyle={l.containerStyle} onPress={l.onPress}>
-          <ListItem.Content style={styles.listItemContent}>
-            <ListItem.Title style={l.titleStyle}>{l.title}</ListItem.Title>
-            <Ionicons
-              style={styles.sendIcon}
-              name="send"
-              size={24}
-              color="white"
-            />
-          </ListItem.Content>
-        </ListItem>
-      ))}
-    </BottomSheet>
+      <TouchableOpacity
+        style={styles.modalBackground}
+        activeOpacity={1}
+        onPress={() => setShowMenu(false)}
+      >
+        <View style={styles.bottomSheet}>
+          {list.map((l, i) => (
+            <ListItem
+              key={i}
+              containerStyle={l.containerStyle}
+              onPress={l.onPress}
+            >
+              <ListItem.Content style={styles.listItemContent}>
+                <ListItem.Title style={l.titleStyle}>{l.title}</ListItem.Title>
+                <TouchableOpacity onPress={() => console.log("AHHHHHHHH")}>
+                  <Ionicons
+                    style={styles.sendIcon}
+                    name="send"
+                    size={24}
+                    color="white"
+                  />
+                </TouchableOpacity>
+              </ListItem.Content>
+            </ListItem>
+          ))}
+        </View>
+      </TouchableOpacity>
+    </Modal>
   );
 };
 
 const styles = StyleSheet.create({
+  modalBackground: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  bottomSheet: {
+    backgroundColor: "#3CB2E2",
+    padding: 16,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
   listItemContent: {
     flexDirection: "row",
     justifyContent: "space-between",
