3f886852ab1ef9a5717de4ac8af18de9a66b9244
masielmartinez <msl.mrtnz.007@gmail.com>
2024-08-06 17:44:17 -0700

    editing disclaimer

--- src/components/Disclaimer.js
+++ src/components/Disclaimer.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { supabase } from "../utils/hooks/supabase";
 import { Dialog } from "@rneui/themed";
+import masielquestion from "../../assets/snapchat/masielquestion.png";
 
 export default function Disclaimer({ isVisible, onClose }) {
   return (
@@ -11,11 +12,33 @@ export default function Disclaimer({ isVisible, onClose }) {
       isVisible={isVisible}
       onBackdropPress={onClose}
     >
-      <Text style={styles.titleText}>Intro to Wellness</Text>
-      <Text style={styles.disclaimerText}>
-        This AI is not a therapist. It is here to help you explore your thoughts
-        and emotions and to aid in self-reflection.
+      <Text style={styles.titleText}>Oh hey! It's MyWellness</Text>
+      <Image style={styles.masielquestion} source={masielquestion} />
+      <Text style={styles.disclaimerHeadText}>
+        Here's what you should know from MyWellness:
       </Text>
+      <Text style={styles.disclaimerText}>
+        This is built on generative Al language models.While we strive for
+        accuracy and quality, please note that the information provided may not
+        be entirely error-free or up-to-date.
+      </Text>
+
+      <Text style={styles.disclaimerText}>
+        We recommend independently verifying the content and consulting with
+        professionals for specific advice or information.
+      </Text>
+
+      <Text style={styles.disclaimerText}>
+        We do not assume any responsibility or liability for the use or
+        interpretation of this content.
+      </Text>
+      <Text style={styles.disclaimerText}>
+        MyWellness content, information, comments, opinions, advice or guidance,
+        programs and activities do not constitute medical or legal advice and
+        are not intended to be relied upon to cure, diagnose or treat any mental
+        or physical health condition.
+      </Text>
+
       <TouchableOpacity onPress={onClose} style={styles.button}>
         <Text style={styles.buttonText}>Start Chat</Text>
       </TouchableOpacity>
@@ -26,22 +49,25 @@ export default function Disclaimer({ isVisible, onClose }) {
 const styles = StyleSheet.create({
   DialogueBox: {
     borderRadius: 20,
+    width: "85%",
+    height: "85%",
   },
   titleText: {
     textAlign: "center",
     fontSize: 23,
     fontWeight: "bold",
-    marginBottom: 20,
+    marginBottom: 10,
   },
   disclaimerText: {
-    textAlign: "center",
+    textAlign: "left",
     fontSize: 15,
-    //fontWeight: "bold",
-    marginBottom: 20,
+    marginBottom: 10,
+    marginLeft: 15,
+    marginRight: 15,
   },
   button: {
     backgroundColor: "#3CB2E2",
-    borderRadius: 5,
+    borderRadius: 30,
     height: 50,
     width: "80%",
     justifyContent: "center",
@@ -53,5 +79,18 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontSize: 16,
     fontWeight: "bold",
+  },
+  disclaimerHeadText: {
+    textAlign: "center",
+    fontSize: 15,
+    //fontWeight: "bold",
+    marginBottom: 20,
+  },
+  masielquestion: {
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: 15,
+    width: "25%",
+    height: "25%",
   },
 });
